5f2922356cd6d619c793c57b1f431a7da56cce60
t-utsunomiya <27965830+t-utsunomiya@users.noreply.github.com>
2019-03-26 21:45:05 +0900

    誤字を修正しました。

--- Source/DiffAssetOpen/Private/DiffAssetOpenCommands.cpp
+++ Source/DiffAssetOpen/Private/DiffAssetOpenCommands.cpp
@@ -9,7 +9,7 @@ void FDiffAssetOpenCommands::RegisterCommands()
 	UI_COMMAND(DiffAssetOpenCommand
 		, "Call DiffAssetOpen Command", "Description: call DiffAssetOpen Command"
 		// なるべく他のキー設定と被らないように EModifierkey を複数設定しています。
-		// 手動で起動するのではなく、ツールを介して DiffAssetPpenDialog を開くことを想定しているので
+		// 手動で起動するのではなく、ツールを介して DiffAssetOpenDialog を開くことを想定しているので
 		// EModifierkey の複数設定は使い勝手の低下につながらないと思います。
 		// EKeys::F12 を設定すると、 EModifierKey と一緒に押したときは正しく動作するものの
 		// F12 単体で押したときに例外が発生したので使用を控えています。
